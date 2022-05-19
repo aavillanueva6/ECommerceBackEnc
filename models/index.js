69f59d47cf9b8b76b1cc998c02c54ebbc793802e
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-19 16:28:46 -0700

    fixes bug due to errantly adding a seedProducts module

--- models/index.js
+++ models/index.js
@@ -3,7 +3,6 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-const seedProducts = require('../seeds/product-seeds');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
@@ -22,7 +21,7 @@ Product.belongsToMany(Tag, {
     model: ProductTag,
     unique: false,
   },
-  as: 'product_tag',
+  as: 'product_tag_1',
 });
 
 // Tags belongToMany Products (through ProductTag)
@@ -31,7 +30,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: 'tag_product',
+  as: 'tag_product_2',
 });
 
 module.exports = {
