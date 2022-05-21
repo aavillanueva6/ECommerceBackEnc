a48a8fc20cebecc1d6b6d56a916e82632011d0c6
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-21 15:22:55 -0700

    adds all products to tag get routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findAll();
+    const tagData = await Tag.findAll({ include: { all: true } });
     res.status(200).json(tagData);
   } catch (err) {
     console.error(err);
@@ -19,7 +19,9 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
-    const tagData = await Tag.findByPk(req.params.id);
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: { all: true },
+    });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id' });
       return;
