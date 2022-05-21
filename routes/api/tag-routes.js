fc5206e73f4a4c74d4c4ce0186f14de5bd699f89
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-05-21 15:50:34 -0700

    adds comments

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,13 +3,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
+// get all tags
 router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
+    // query database for all data in the tag table, include all related data
     const tagData = await Tag.findAll({ include: { all: true } });
+    // send successful response
     res.status(200).json(tagData);
   } catch (err) {
+    // if there is an error, log it and send an error response
     console.error(err);
     res.status(500).json(err);
   }
@@ -19,15 +23,19 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
+    // query database for aspecific row of data in the tag table, include all related data
     const tagData = await Tag.findByPk(req.params.id, {
       include: { all: true },
     });
+    // return a 404 and error message if the queried id doesn't exist
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id' });
       return;
     }
+    // send successful response
     res.status(200).json(tagData);
   } catch (err) {
+    // if there is an error, log it and send an error response
     console.error(err);
     res.status(500).json(err);
   }
@@ -35,10 +43,13 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req, res) => {
   // create a new tag
+  // query db to create a new row in the tag table with the request body
   try {
     const tagData = await Tag.create(req.body);
+    // send successful response
     res.status(200).json(tagData);
   } catch (err) {
+    // if there is an error, log it and send an error response
     console.error(err);
     res.status(500).json(err);
   }
@@ -47,18 +58,22 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
+    // query db to see if the requested tag exists, if it doesn't return 404 and error message
     const tag = await Tag.findByPk(req.params.id);
     if (!tag) {
       res.status(404).json({ message: 'No tag found with this id' });
       return;
     }
 
+    // query db to update the requested row in the tag table with the request body
     const tagData = await Tag.update(
       { tag_name: `${req.body.tag_name}` },
       { where: { id: req.params.id } }
     );
+    // send successful response
     res.status(200).json(tagData);
   } catch (err) {
+    // if there is an error, log it and send an error response
     console.error(err);
     res.status(500).json(err);
   }
@@ -67,14 +82,18 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
+    // query db to see if the requested tag exists, if it doesn't return 404 and error message
     const tag = await Tag.findByPk(req.params.id);
     if (!tag) {
       res.status(404).json({ message: 'No tag found with this id' });
       return;
     }
+    // query db to delete the requested row in the tag table
     const tagData = await Tag.destroy({ where: { id: req.params.id } });
+    // send successful response
     res.status(200).json(tagData);
   } catch (err) {
+    // if there is an error, log it and send an error response
     console.error(err);
     res.status(500).json(err);
   }
